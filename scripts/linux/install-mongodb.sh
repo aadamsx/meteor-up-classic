df9b7b2708b9019fb9da813e8e8fc7111c569ab3
aadamsx <aadams99@gmail.com>
2016-08-06 20:46:30 -0500

    version 0.12.0

---
+++ scripts/linux/install-mongodb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Remove the lock
+set +e
+sudo rm /var/lib/dpkg/lock > /dev/null
+sudo rm /var/cache/apt/archives/lock > /dev/null
+sudo dpkg --configure -a
+set -e
+
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+#echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+sudo apt-get update -y
+sudo apt-get install mongodb-org mongodb-org-server mongodb-org-shell mongodb-org-tools -y
+
+# Restart mongodb
+sudo stop mongod || :
+sudo start mongod
